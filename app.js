08a4de260cb93b392153ddb371a52449ca0ead50
zhouxu <yawang@163.com>
2018-11-06 21:17:20 +0800

    循环注册路由模块完成

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const app = express()
 const bodyParser =require('body-parser')
-
+const fs =require('fs')
+const path=require('path')
 app.set('view engine','ejs')
 app.set('views','./views')
 // 注册解析表单数据的中间件
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/node_modules',express.static('./node_modules'))
-// 用户请求的是首页
-const router1 =require('./router/index.js')
-app.use(router1)
-// 用户请求的是用户页面
-const router2 =require('./router/user.js')
-app.use(router2)
+// 注册路由模块
+// const router1 =require('./router/index.js')
+// app.use(router1)
+
+// const router2 =require('./router/user.js')
+// app.use(router2)
+
+// 使用 循环的方式，进行路由的自动注册
+fs.readdir(path.join(__dirname, './router'), (err, filenames) => {
+    if (err) return console.log('读取 router 目录中的路由失败！')
+    // 循环router目录下的每一个文件名
+    filenames.forEach(fname => {
+      // 每循环一次，拼接出一个完整的路由模块地址
+      // 然后，使用 require 导入这个路由模块
+      const router = require(path.join(__dirname, './router', fname))
+      app.use(router)
+    })
+  })
 app.listen(80,()=>{
     console.log('server running at http://127.0.0.1')
 })
