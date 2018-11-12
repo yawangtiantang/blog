92a4476215e77555c3ac426cb1b93a070e9cd9d2
zhouxu <yawang@163.com>
2018-11-12 21:56:21 +0800

    对用户密码进行加密

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,10 @@
 const moment = require('moment')
 const conn = require('../db/db.js')
+// 导入加密模块
+const bcrypt = require('bcrypt')
+// 定义一个 幂次
+const saltRounds = 10 // 2^10
+
 module.exports = {
   handleRegisterGet(req, res) {
 
@@ -37,9 +42,15 @@ module.exports = {
       console.log(result.msg)
       // 给用户添加创建时间的属性
       user.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+      // 在执行Sql语句之前，先对用户提供的密码，做一层加密，防止密码被泄露之后，明文被盗取的清空
+    // bcrypt.hash('要被加密的密码', 循环的幂次, 回调函数)
+    bcrypt.hash(user.password, saltRounds, (err,pwd) =>{
+      if(err) return res.send({status:506,msg:'注册用户失败'})
+      // 把加密之后的新密码，赋值给 body.password
+      user.password = pwd
       // 执行注册的用户逻辑
       const addSql = 'insert into users set?'
-      console.log(user)
+      // console.log(user)
       conn.query(addSql, user, (err, result) => {
         if (err || result.affectedRows != 1) return res.status(500).send({
           status: 500,
@@ -53,14 +64,46 @@ module.exports = {
 
     })
 
+    })
+      
   },
 
   handleLoginPost(req, res) {
-    const user = req.body
+    // const user = req.body
+    // //    console.log(body)
+    // // 查询执行sql语句
+    // const sql = 'select * from users where username=? and password=?'
+    // conn.query(sql, [user.username, user.password], (err, result) => {
+    //   if (err) return res.send({
+    //     status: 501,
+    //     msg: '用户登录失败'
+    //   })
+    //   // console.log(result)
+    //   if (result.length !== 1) return res.send({
+    //     status: 502,
+    //     msg: '用户登录失败'
+    //   })
+    //   // console.log(req.session)
+    //   // console.log(result)
+    //  // 登录成功后存储用户信息到session中
+    //  req.session.user = result[0]
+    //  // 存储登录状态
+    //  req.session.isLogin = true
+    // //  设置cookie的有效期
+    // let hour = 1000*60*60*24*30
+    // req.session.cookie.expires=new Date(Date.now()+hour)
+    //   res.send({
+    //     status: 200,
+    //     msg: '登录成功'
+    //   })
+    // })
+
+
+const user = req.body
     //    console.log(body)
     // 查询执行sql语句
-    const sql = 'select * from users where username=? and password=?'
-    conn.query(sql, [user.username, user.password], (err, result) => {
+    const sql = 'select * from users where username=? '
+    conn.query(sql, [user.username], (err, result) => {
       if (err) return res.send({
         status: 501,
         msg: '用户登录失败'
@@ -70,6 +113,14 @@ module.exports = {
         status: 502,
         msg: '用户登录失败'
       })
+      // 对比 密码的方法
+    // bcrypt.compare('用户输入的密码', '数据库中记录的密码', 回调函数)
+    bcrypt.compare(user.password,result[0].password,(err,compareResult)=>{
+        if(err) return res.send({status:503,msg:'登录失败'})
+        if(!compareResult) return res.send({ msg: '用户登录失败', status: 504 })
+    })
+      
+      
       // console.log(req.session)
       // console.log(result)
      // 登录成功后存储用户信息到session中
@@ -84,6 +135,7 @@ module.exports = {
         msg: '登录成功'
       })
     })
+
 
   },
   handleLogoutGet(req, res) {
